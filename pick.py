08e057bc0df195c9c5cfe875ccbcf9c70a015491
Jeffrey Farley <JeffreyMFarley@users.noreply.github.com>
2017-10-11 09:12:59 -0400

    Use absolute not relative path

--- pick.py
+++ pick.py
@@ -3,11 +3,18 @@
 import random
 
 
+def to_absolute(fileName):
+    import os.path
+    # where is this module?
+    thisDir = os.path.dirname(__file__)
+    return os.path.join(thisDir, fileName)
+
+
 def load(fileName):
     with open(fileName, 'r') as f:
         for line in f:
             yield line.strip()
 
 if __name__ == '__main__':
-    words = [l for l in load('brutethink.txt')]
+    words = [l for l in load(to_absolute('brutethink.txt'))]
     print(random.choice(words))
